60e4ead78ba651cc872eca7cfbd07e1e11b1a3fb
Flavio Pirola <flavio.pirola@cristalcopo.com.br>
2024-10-28 12:42:54 -0300

    inserindo título e rodapé

--- src/Pages/homepage/index.tsx
+++ src/Pages/homepage/index.tsx
@@ -4,7 +4,16 @@ import './index.css';
 function Homepage() {
   return (
     <div className="Homepage">
-      <h1>Homepage</h1>
+      <h1 className='Nome_inicial'>Flávio Leandro Pirola</h1>
+
+      <section className='section_descrição'>
+
+        <div className='t'>xxxxxxxxx</div>
+
+
+      </section>
+
+      <h1 className='footer'>Desenvolvido por Flávio - 2024</h1>
     </div>
   );
 }
